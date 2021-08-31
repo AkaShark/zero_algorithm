3776ce6be33991660ddfbbf2f9b47959c14f0271
Alloc Alice <1548742234@qq.com>
2021-08-31 22:33:47 +0800

    Update MissingNumber.java
    我这样的二分感觉更舒服

--- missingnumber/src/MissingNumber.java
+++ missingnumber/src/MissingNumber.java
@@ -14,4 +14,18 @@ public class MissingNumber {
         }
         return left;
     }
+    
+    public static int missingNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int middle = (right - left) / 2 + left;
+            if (middle == nums[middle]) { // 左边排除
+                left = middle + 1;
+            } else { // 右边排除
+                right = middle;
+            }
+        }
+        return left;
+    }
 }
